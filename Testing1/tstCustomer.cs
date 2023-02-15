ef1782fe445a322cbb56c848880ab4f2f31c5bef
P2655547 <P2655547@my365.dmu.ac.uk>
2023-02-15 21:07:44 +0000

    Created test for instaciating class, also created class file.

---
+++ Testing1/tstCustomer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstCustomer
+    {
+        [TestMethod]
+        public void InstanceOK(){
+            clsCustomer Customer = new clsCustomer();
+            Assert.IsNotNull(Customer);
+        }
+    }
+}
