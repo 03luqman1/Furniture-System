7752cff031f97993aa0ca421880e6b600199c9c7
P2655547 <P2655547@my365.dmu.ac.uk>
2023-02-15 21:53:23 +0000

    Created tests for each property of the customer table. All work and pass.

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
-    class clsCustomer
+    public class clsCustomer
     {
+        public DateTime DateOfBirth { get; set; }
+        public bool Verified { get; set; }
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
