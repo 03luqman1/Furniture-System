f6161027df39889767772349bd2e4dbbf713f429
Shahzaib Ali <P2663950@my365.dmu.ac.uk>
2023-02-23 14:53:33 +0000

    Make the order testing class

---
+++ Testing2/tstOrder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
