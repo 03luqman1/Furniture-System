6385461735663c84bca8071828c831e877f0db7e
Sheraz Afzal <P2713718@my365.dmu.ac.uk>
2023-03-16 23:38:27 +0000

    testing the propreties of staff table

--- Testing3/tstStaff.cs
+++ Testing3/tstStaff.cs
@@ -15,5 +15,72 @@ namespace Testing3
             //test to see it exists
             Assert.IsNotNull(AnEmployee);
         }
+        [TestMethod]
+        public void EmployeeIDPropertyOK()
+        {
+            // create an instance of the class we went to create
+            clsEmployee Employee = new clsEmployee();
+            // create some test data to assign to the property
+            Int32 TestData = 479921;
+            // assign the data to the property
+            Employee.EmployeeID = TestData;
+            // test to see that the two values are the same
+            Assert.AreEqual(Employee.EmployeeID, TestData);
+        }
+        [TestMethod]
+        public void NamePropertyOK()
+        {
+            clsEmployee Employee = new clsEmployee();
+            string Testdata = "Babar Azam";
+            Employee.Name = Testdata;
+            Assert.AreEqual(Employee.Name, Testdata);
+        }
+        [TestMethod]
+        public void ContentNumberPropertyOK()
+        {
+            clsEmployee Employee = new clsEmployee();
+            string TestData = "07428429221";
+            Employee.ContentNumber = TestData;
+            Assert.AreEqual(Employee.ContentNumber, TestData);
+
+        }
+        [TestMethod]
+        public void JobPostionPropertyOK()
+        {
+            clsEmployee Employee = new clsEmployee();
+            string TestData = "Manager";
+            Employee.JobPostion = TestData;
+            Assert.AreEqual(Employee.JobPostion, TestData);
+        }
+        [TestMethod]
+        public void EmployeeSalaryPropertyOK()
+        {
+            clsEmployee Employee = new clsEmployee();
+            decimal TestData = 4000;
+            Employee.EmployeeSalary = TestData;
+            Assert.AreEqual(Employee.EmployeeSalary, TestData);
+
+        }
+        [TestMethod]
+        public void StartDatePropertyOK()
+        {
+            clsEmployee Employee = new clsEmployee();
+            DateTime TestData = DateTime.Now.Date;
+            Employee.StartData = TestData;
+            Assert.AreEqual(Employee.StartData, TestData);
+
+        }
+        [TestMethod]
+        public void CurrentEmployeeStatusPropertyOK()
+        {
+            clsEmployee Employee = new clsEmployee();
+            Boolean TestData = true;
+            Employee.CurrentEmployeeStatus = TestData;
+            Assert.AreEqual(Employee.CurrentEmployeeStatus, TestData);
+
+        }
+
+
+
     }
 }
