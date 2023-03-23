eb68a1b2a9c4b050c77b9ab35f8a52897ea6c1a9
Shahzaib Ali <P2663950@my365.dmu.ac.uk>
2023-03-23 14:44:15 +0000

    test the properties of order table

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,6 +1,14 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
+        public bool orderConfirm;
+
+        public int OrderId { get; set; }
+        public int OrderQuantity { get; set; }
+        public DateTime OrderDate { get; set; }
+        public bool OrderStatus { get; set; }
     }
 }
