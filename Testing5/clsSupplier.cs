b56e615c71e887cc620a0c734def40ebaa30e707
Sheraz Afzal <P2713718@my365.dmu.ac.uk>
2023-03-30 16:32:40 +0100

    first test

---
+++ Testing5/clsSupplier.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstSupplier
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create the an instance of the class we want to create
+            clsSupplier AnSupplier = new clsSupplier();
+            //test to see it exists
+            Assert.IsNotNull(AnSupplier);
+        }
+    }
+
+    class clsSupplier
+    {
+    }
+}
